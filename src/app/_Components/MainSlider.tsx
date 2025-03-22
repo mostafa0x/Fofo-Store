ffe57b6c4706d951ee335a79297247b2dfa6dc17
mostafa0x <dodomido12350@gmail.com>
2025-03-22 02:57:05 -0700

    Improve the main slider

--- src/app/_Components/MainSlider.tsx
+++ src/app/_Components/MainSlider.tsx
@@ -1,47 +1,85 @@
-import React, { useContext } from 'react'
-import Slider from "react-slick";
+'use client';
+import React, { useContext } from 'react';
 import { CategoriesContext } from '../_Contexts/CategoriesContext';
-import Image from 'next/image';
-
-
+import Slider from 'react-slick';
 
 export default function MainSlider() {
-    const { Categories } = useContext(CategoriesContext)
-    var settings = {
-        dots: false,
-        infinite: false,
+    const { Categories } = useContext(CategoriesContext);
+
+    const settings = {
+        dots: true,
+        infinite: true,
         speed: 500,
-        slidesToShow: 6,
-        slidesToScroll: 6,
-        autoplaySpeed: 1500,
-        autoplay: false,
-        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
     };
+
     return (
-        <div className='z-[0] p-24'>
-            <div className="grid grid-cols-3 gap-6">
+        <div className="bg-gray-100 py-16">
+            {/* العنوان */}
+            <h1 className="text-center text-3xl font-semibold text-gray-800 mb-8">
+                Explore Our Best Products
+            </h1>
 
-                {/* <Image
-                    src="/img/test"
-                    alt="Bag Image"
-                    width={100}
-                    height={60}
-                    className="w-full h-60 object-fill"
-                /> */}
+            <div className="max-w-screen-xl mx-auto px-4">
+                {/* ترتيب الصور: تقسيم الى جزءين */}
+                <div className="grid grid-cols-2 gap-8">
 
-                {/* <Image src="https://s3.us-west-1.amazonaws.com/shop.egyantiques-bucket/wp-content/uploads/2023/02/16023231/%D8%AA%D8%AD%D9%81-%D9%88%D8%A7%D9%86%D8%AA%D9%8A%D9%83%D8%A7-%D8%A7%D9%8A%D8%AC%D9%8A%D8%A8%D8%AA-%D8%A7%D9%86%D8%AA%D9%8A%D9%83%D8%B3.jpg" alt="" className="w-full h-60 object-fill " />
-                <Image src="https://s3.us-west-1.amazonaws.com/shop.egyantiques-bucket/wp-content/uploads/2023/02/16023233/%D8%A7%D8%AC%D9%8A%D8%A8%D8%AA-%D8%A7%D9%86%D8%AA%D9%8A%D9%83%D8%B3-%D8%A7%D9%84%D9%83%D9%84%D9%8A%D9%85.jpg" alt="" className="w-full h-60 object-contain " />
-                <Image src="https://s3.us-west-1.amazonaws.com/shop.egyantiques-bucket/wp-content/uploads/2023/02/16023234/wiker1.jpg" alt="" className="w-full h-60  object-contain" />
-                <Image src="https://s3.us-west-1.amazonaws.com/shop.egyantiques-bucket/wp-content/uploads/2023/02/16023235/Salt-lamp.jpg" alt="" className="w-full h-60 object-contain " />
+                    {/* الجزء الأيسر: صورتين فوق بعض */}
+                    <div className="flex flex-col gap-4 w-full">
+                        {/* الصورة الأولى */}
+                        <div className="relative overflow-hidden rounded-lg shadow-lg transform hover:scale-105 transition-all duration-500 ease-in-out">
+                            <img
+                                src="https://img.freepik.com/free-photo/black-friday-elements-assortment_23-2149074075.jpg"
+                                alt="Product 1"
+                                className="w-full h-48 object-cover rounded-lg"
+                            />
+                            <div className="absolute bottom-0 left-0 p-5 text-white bg-black bg-opacity-50 w-full text-center">
+                                <h2 className="text-lg font-semibold">Product 1</h2>
+                            </div>
+                        </div>
 
-                <Image src="https://s3.us-west-1.amazonaws.com/shop.egyantiques-bucket/wp-content/uploads/2023/09/22045447/%D8%B9%D8%B1%D9%88%D8%B6-%D9%83%D9%84-%D8%AC%D9%85%D8%B9%D8%A9-.jpg" alt="" className="w-[1550] h-60 object-contain " /> */}
+                        {/* الصورة الثانية */}
+                        <div className="relative overflow-hidden rounded-lg shadow-lg transform hover:scale-105 transition-all duration-500 ease-in-out">
+                            <img
+                                src="https://blog.thinkingschool.vn/wp-content/uploads/2021/01/Shopping.jpg"
+                                alt="Product 2"
+                                className="w-full h-48 object-cover rounded-lg"
+                            />
+                            <div className="absolute bottom-0 left-0 p-5 text-white bg-black bg-opacity-50 w-full text-center">
+                                <h2 className="text-lg font-semibold">Product 2</h2>
+                            </div>
+                        </div>
+                    </div>
 
+                    {/* الجزء الأيمن: صورة كبيرة مع Slider */}
+                    <div className="w-full">
+                        <div className="relative overflow-hidden rounded-lg shadow-lg transform hover:scale-105 transition-all duration-500 ease-in-out">
+                            {/* هذا هو Slider للصورة الكبيرة */}
+                            <Slider {...settings}>
+                                <img
+                                    src="https://www.searchenginejournal.com/wp-content/uploads/2022/08/google-shopping-ads-6304dccb7a49e-sej-1280x720.png"
+                                    alt="Main Product"
+                                    className="w-full h-full object-cover rounded-lg"
+                                />
+                                <img
+                                    src="https://img.freepik.com/free-photo/modern-creative-glass-jar-with-orange-fruit-pulp_1150-5567.jpg"
+                                    alt="Main Product 2"
+                                    className="w-full h-full object-cover rounded-lg"
+                                />
+                                <img
+                                    src="https://www.expertreviews.co.uk/sites/expertreviews/files/2021/05/Best-Kitchen-Appliances-Under-100.jpg"
+                                    alt="Main Product 3"
+                                    className="w-full h-full object-cover rounded-lg"
+                                />
+                            </Slider>
+                            <div className="absolute bottom-0 left-0 p-5 text-white bg-black bg-opacity-50 w-full text-center">
+                                <h2 className="text-xl font-semibold">Main Product</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
-
-
-
-
-    )
+    );
 }
