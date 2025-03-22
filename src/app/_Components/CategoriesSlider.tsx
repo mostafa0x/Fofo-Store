ab6793ae830c487ec3d7ba1b483a939230f32580
mostafa0x <dodomido12350@gmail.com>
2025-03-22 04:26:27 -0700

    Improve product display page by category

--- src/app/_Components/CategoriesSlider.tsx
+++ src/app/_Components/CategoriesSlider.tsx
@@ -29,11 +29,11 @@ export default function CategoriesSlider() {
             <div className="flex justify-center">
                 <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 w-full px-48 sm:px-8 lg:pl-48">
                     {Categories?.slice(1).map((Category: CategoryType, index: number) => (
-                        <div
-                            key={index}
-                            className="group relative bg-white border border-gray-200 p-4 rounded-2xl shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-xl hover:bg-gray-100 cursor-pointer"
-                        >
-                            <Link href={`/Category/${Category.name}`}>
+                        <Link key={index} href={`/Category/${Category.name}`}>
+                            <div
+                                className="group relative bg-white border border-gray-200 p-4 rounded-2xl shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-xl hover:bg-gray-100 cursor-pointer"
+                            >
+
                                 <img
                                     src={Category.image}
                                     alt={Category.name}
@@ -42,10 +42,11 @@ export default function CategoriesSlider() {
                                 <h2 className="mt-4 text-xl font-medium text-gray-800 group-hover:text-blue-600 transition-all duration-200">
                                     {Category.name}
                                 </h2>
-                            </Link>
 
-                            <div className="absolute inset-0 bg-black bg-opacity-30 group-hover:bg-opacity-0 transition-all duration-300 rounded-xl"></div>
-                        </div>
+                                <div className="absolute inset-0 bg-black bg-opacity-30 group-hover:bg-opacity-0 transition-all duration-300 rounded-xl"></div>
+                            </div>
+                        </Link>
+
                     ))}
                 </div>
             </div>
