3e7ef1074eaaf2820d71e204066c5ce6a8a6926b
mostafa0x <dodomido12350@gmail.com>
2025-03-23 08:21:42 -0700

    Improve screen dimensions for some pages

--- src/app/_Components/CategoriesSlider.tsx
+++ src/app/_Components/CategoriesSlider.tsx
@@ -18,13 +18,10 @@ export default function CategoriesSlider() {
             </h1>
 
             <div className="flex justify-center">
-                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 w-full px-48 sm:px-8 lg:pl-48">
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 w-full px-4 sm:px-8 lg:px-16">
                     {Categories?.slice(1).map((Category: CategoryType, index: number) => (
                         <Link key={index} href={`/Category/${Category.name}`}>
-                            <div
-                                className="group relative bg-white border border-gray-200 p-4 rounded-2xl shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-xl hover:bg-gray-100 cursor-pointer"
-                            >
-
+                            <div className="group relative bg-white border border-gray-200 p-4 rounded-2xl shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-xl hover:bg-gray-100 cursor-pointer">
                                 <img
                                     src={Category.image}
                                     alt={Category.name}
@@ -37,7 +34,6 @@ export default function CategoriesSlider() {
                                 <div className="absolute inset-0 bg-black bg-opacity-30 group-hover:bg-opacity-0 transition-all duration-300 rounded-xl"></div>
                             </div>
                         </Link>
-
                     ))}
                 </div>
             </div>
