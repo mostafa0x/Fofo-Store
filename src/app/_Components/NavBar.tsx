eb7717ecef1c671ae857b656ff543b6fea78c0cf
mostafa0x <dodomido12350@gmail.com>
2025-03-23 22:08:15 -0700

    Navbar improvements v2.1

--- src/app/_Components/NavBar.tsx
+++ src/app/_Components/NavBar.tsx
@@ -57,11 +57,9 @@ export default function NavBar() {
 
     }, [Path]);
 
-    useEffect(() => {
-        HookCart.refetch()
-
-
-    }, [session])
+    // useEffect(() => {
+    //     HookCart.refetch()
+    // }, [session])
 
     function LogOut() {
         if (TV === -1) {
@@ -76,21 +74,27 @@ export default function NavBar() {
             toast.error("Enter Text to Search bar")
         } else {
             setSearchTXT(SecTXT)
+            console.log(CurrentCategory?.name);
 
+            if (typeof CurrentCategory === 'undefined') {
+                return Router.push(`/Search/All/${SecTXT}`)
+            }
             Router.push(`/Search/${CurrentCategory?.name}/${SecTXT}`)
         }
     }
 
     function SetCategory(NameCategory: string) {
-        console.log(NameCategory);
-
         const Category = Categories.find((category) => {
             return category.name == NameCategory
         })
         setCurrentCategory(Category)
     }
 
-
+    function List() {
+        if (CartList) {
+            CartList.current.tabIndex = -1
+        }
+    }
 
     return (
         <div className="navbar fixed top-0 bg-base-100 shadow-sm z-[60]">
@@ -104,10 +108,10 @@ export default function NavBar() {
                         <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                     </svg>
                     <span className="sr-only">Loading...</span>
-                </div> : <div ref={CartList} className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle pr-4">
+                </div> : <div className="dropdown">
+                    <div ref={CartList} tabIndex={0} role="button" className="btn btn-ghost btn-circle pr-4">
                         <div className="indicator items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-15 w-8 items-center" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="flex h-15 w-8 text-center items-center" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
                             <span className="badge badge-sm indicator-item">{MyCart.MyCart.length}</span>
                         </div>
                     </div>
@@ -116,9 +120,9 @@ export default function NavBar() {
                         className="dropdown-content menu bg-base-100 z-1 mt-3 w-52 shadow">
                         <div className="card-body">
                             <span className="text-lg font-bold">{MyCart.MyCart.length} Items</span>
-                            <span className="text-info">Subtotal: {MyCart.Totalprice} EGP</span>
+                            <span className="text-info">Total Price: {MyCart.Totalprice} EGP</span>
                             <div className="card-actions">
-                                <Link href={"/Cart"}><button className="btn btn-primary btn-block">View cart</button></Link>
+                                <Link onClick={() => List()} href={"/Cart"}><button className="btn btn-primary btn-block">View cart</button></Link>
                             </div>
                         </div>
                     </div>
@@ -135,13 +139,16 @@ export default function NavBar() {
                         <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                     </svg>
                     <span className="sr-only">Loading...</span>
-                </div> : <select ref={Selector} defaultValue="All" className="select " onClick={(e) => {
-                    SetCategory(e.currentTarget.value)
-                }}>
-                    {Categories.map((category, index) => {
-                        return <option key={index} >{category.name}</option>
-                    })}
-                </select>}
+                </div> :
+                    <select ref={Selector} defaultValue="All" className="select " onClick={(e) => {
+                        SetCategory(e.currentTarget.value)
+                    }}>
+                        {Categories.map((category, index) => {
+                            return <option key={index} >{category.name}</option>
+                        })}
+                    </select>
+
+                }
 
                 <button onClick={() => { Search(SearchBar?.current?.value) }} className="btn btn-ghost btn-circle">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> </svg>
@@ -168,8 +175,8 @@ export default function NavBar() {
                     {UserToken ?
                         <ul
                             tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <li onClick={() => LogOut()}><i>Logout</i></li>
+                            className="menu menu-sm dropdown-content text-3xl font-semibold bg-gray-300 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            <li className=' rounded-xl hover:bg-slate-600' onClick={() => LogOut()}><i>Logout</i></li>
                         </ul>
                         : null}
                 </div>}
