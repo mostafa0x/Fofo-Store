2f983d928234308ec2a2c4f3db8eac111974b1f8
mostafa0x <dodomido12350@gmail.com>
2025-03-24 02:25:29 -0700

    Fix Footer

--- src/app/_Components/Footer.tsx
+++ src/app/_Components/Footer.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <div id="app">
-            <footer className="bg-gray-800 py-4 z-[54]">
+        <div id="app" className="mt-20 flex flex-col">
+            <footer className="bg-gray-800 py-4 mt-auto">
                 <div className="max-w-screen-xl mx-auto px-2">
-                    <div className="flex justify-between">
+                    <div className="flex justify-between items-center">
                         <div className='text-white'>
                             <h3 className="text-2xl font-semibold">Fofo Store</h3>
-                            <p className="text-sm">Shoping</p>
+                            <p className="text-sm">Shopping</p>
                         </div>
                         <div>
                             <div className="text-left">
@@ -17,21 +17,20 @@ export default function Footer() {
                                 <span className='text-gray-400'>Follow us</span>
                             </div>
 
-                            <div className='text-3xl flex gap-2 pt-4'>
+                            <div className='text-3xl flex gap-4 pt-4'>
                                 <a href='https://www.facebook.com/profile.php?id=100080751632307' target='_blank' className="fa-brands fa-facebook cursor-pointer hover:text-yellow-300" />
                                 <a href='https://www.instagram.com/mostafa_0x/' target='_blank' className="fa-brands fa-instagram hover:text-yellow-300" />
-
                             </div>
                         </div>
                     </div>
 
                     <div className="mt-4 text-sm text-white">
-                        <div className='flex justify-center gap-4 text-3xl text-green-500'>
+                        <div className='flex justify-center gap-6 text-3xl text-green-500'>
                             <i className="fa-brands fa-cc-visa" ></i>
                             <i className="fa-brands fa-cc-mastercard" />
                             <i className="fa-brands fa-cc-paypal" />
                         </div>
-                        <p>© Fofo Store. All rights reserved</p>
+                        <p className="text-center mt-2">© Fofo Store. All rights reserved</p>
                     </div>
                 </div>
             </footer>
